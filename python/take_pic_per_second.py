44601ce859ee4fba284c6c1b4e13709f19f061ce
arukan02 <87008720+arukan02@users.noreply.github.com>
2022-12-05 15:09:38 +0900

    毎秒に写真を撮る

---
+++ python/take_pic_per_second.py
@@ -0,0 +1,142 @@
+#have to determine the ROI first - determined by face
+#Take_picture_per_second.py
+
+from time import time
+import cv2
+import sys
+
+img_counter = 0
+
+(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+ 
+if __name__ == '__main__' :
+ 
+    # Set up tracker.
+    # Instead of CSRT, you can also use
+ 
+    tracker_types = ['BOOSTING', 'MIL','KCF', 'TLD', 'MEDIANFLOW', 'GOTURN', 'MOSSE', 'CSRT']
+    tracker_type = tracker_types[1]  #BOOSTING
+ 
+    if int(minor_ver) < 3:
+        tracker = cv2.Tracker_create(tracker_type)
+    else:
+        if tracker_type == 'BOOSTING':
+            tracker = cv2.TrackerBoosting_create()
+        elif tracker_type == 'MIL':
+            tracker = cv2.TrackerMIL_create()
+        elif tracker_type == 'KCF':
+            tracker = cv2.TrackerKCF_create()
+        elif tracker_type == 'TLD':
+            tracker = cv2.TrackerTLD_create()
+        elif tracker_type == 'MEDIANFLOW':
+            tracker = cv2.TrackerMedianFlow_create()
+        elif tracker_type == 'GOTURN':
+             tracker = cv2.TrackerGOTURN_create()
+        elif tracker_type == 'MOSSE':
+            tracker = cv2.TrackerMOSSE_create()
+        elif tracker_type == "CSRT":
+            tracker = cv2.TrackerCSRT_create()
+            
+# Read video
+#video = cv2.VideoCapture("input.mp4")
+video = cv2.VideoCapture(0) # for using CAM
+
+# Exit if video not opened.
+if not video.isOpened():
+    print("Could not open video")
+    sys.exit()
+
+# Read first frame.
+ok, frame = video.read()
+if not ok:
+    print ('Cannot read video file')
+    sys.exit()
+    
+# import cascade file for facial recognition
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+
+frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+# Getting corners around the face
+faces = faceCascade.detectMultiScale(frameGray, 1.3, 5)  # 1.3 = scale factor, 5 = minimum neighbor
+# drawing bounding box around face
+for (x, y, w, h) in faces:
+    frame = cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
+    # Define an initial bounding box
+    bbox = (x, y, w, h) #287, 23, 86, 320
+
+
+
+# Uncomment the line below to select a different bounding box
+#bbox = cv2.selectROI(frame, False)
+
+# Initialize tracker with first frame and bounding box
+ok = tracker.init(frame, bbox)
+
+# Initialise variables to store current time difference as well as previous time call value
+previous = time()
+delta = 0
+
+while True:
+     # Read a new frame
+    ok, frame = video.read()
+    if not ok:
+        break
+      
+     # Start timer
+    timer = cv2.getTickCount()
+
+     # Update tracker
+    ok, bbox = tracker.update(frame)
+
+     # Calculate Frames per second (FPS)
+    fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer);
+
+     # Draw bounding box
+    if ok:
+         # Tracking success
+        p1 = (int(bbox[0]), int(bbox[1]))
+        p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+        cv2.rectangle(frame, p1, p2, (255,0,0), 2, 1)
+    else :
+        # Tracking failure
+        cv2.putText(frame, "Tracking failure detected", (100,80), cv2.FONT_HERSHEY_SIMPLEX, 0.75,(0,0,255),2)
+
+    # Display tracker type on frame
+    cv2.putText(frame, tracker_type + " Tracker", (100,20), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50),2);
+  
+    # Display FPS on frame
+    cv2.putText(frame, "FPS : " + str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (50,170,50), 2);
+    
+     # Get the current time, increase delta and update the previous variable
+    current = time()
+    delta += current - previous
+    previous = current
+
+    # Check if 3 (or some other value) seconds passed
+    if delta > 1:
+        # Save image
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+        # Reset the time counter
+        delta = 0
+        
+    # Display result
+    cv2.imshow("Tracking", frame)
+
+    # Exit if ESC pressed
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed, take picture
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+video.release()
+cv2.destroyAllWindows()
