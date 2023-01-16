58c39b8011324ce17960326f5491266800465c3b
arukan02 <alcanderimawan@gmail.com>
2023-01-16 14:30:07 +0900

    MariaDBコネクションテスト

--- python/connect_DB.py
+++ python/connect_DB.py
@@ -1,31 +1,27 @@
-import os
+# Module Imports
+import mariadb
+import sys
 
-import sqlalchemy
+# Connect to MariaDB Platform
+try:
+    conn = mariadb.connect(
+        user="testing",
+        password="",
+        host="35.230.86.157",
+        port=3306,
+        database="moka_test"
 
-
-def connect_tcp_socket() -> sqlalchemy.engine.base.Engine:
-    """ Initializes a TCP connection pool for a Cloud SQL instance of MySQL. """
-    # Note: Saving credentials in environment variables is convenient, but not
-    # secure - consider a more secure solution such as
-    # Cloud Secret Manager (https://cloud.google.com/secret-manager) to help
-    # keep secrets safe.
-    db_host = os.environ["35.230.86.157"]  # e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
-    db_user = os.environ["testing"]  # e.g. 'my-db-user'
-    db_pass = os.environ[""]  # e.g. 'my-db-password'
-    db_name = os.environ["testing"]  # e.g. 'my-database'
-    db_port = os.environ["3306"]  # e.g. 3306
-
-    pool = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@<db_host>:<db_port>/<db_name>
-        sqlalchemy.engine.url.URL.create(
-            drivername="mysql+pymysql",
-            username=db_user,
-            password=db_pass,
-            host=db_host,
-            port=db_port,
-            database=db_name,
-        ),
-        # ...
     )
-    return pool
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+# Get Cursor
+cur = conn.cursor()
+
+cur.execute(
+    "SELECT * FROM users")
+
+# Print Result-set
+for row in cur:
+   print(row)
