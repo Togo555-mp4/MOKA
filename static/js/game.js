a332246043175556e9ec1be32cd90f6c547773f1
Togo555-mp4 <5t1ng20X@gmail.com>
2023-01-21 17:11:32 +0900

    ミスの修正

--- static/js/game.js
+++ static/js/game.js
@@ -15,7 +15,7 @@ function playerGet(){
 // 1秒ごとに行うもの
 setInterval(() => {
     // 画面表示について
-    picturePost();
+    // picturePost();
     // pictureGet();
     // 回答について
     answerGet('http://35.230.86.157/answerGet');
