e22dedeb9482d63a740c62dc84ea139cd81b5507
Togo555-mp4 <5t1ng20X@gmail.com>
2023-01-21 16:12:23 +0900

    ミスの修正

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 from flask import Flask, render_template, request
 # base64のインポート
 import base64
-from .python import connect_Maria 
+from python import connect_Maria 
 
 # Flaskオブジェクトの生成
 app = Flask(__name__)
