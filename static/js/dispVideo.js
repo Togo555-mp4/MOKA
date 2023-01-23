80243703c43c06c6dfb2f36e5049a2da0ea2265c
Togo555-mp4 <5t1ng20X@gmail.com>
2023-01-23 15:44:20 +0900

    画像の通信

--- static/js/dispVideo.js
+++ static/js/dispVideo.js
@@ -16,17 +16,17 @@ function picturePost(){
     // video要素の映像をcanvasに描画する
     canvasCtx.drawImage(video, 0, 0, canvas.width, canvas.height);
     let base64 = this.canvas.toDataURL();
-    let postPicture = base64.replace("/^data:\w+/\w+;base64,/", "");
-    fetch(postUrl, {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json' },
-        body: JSON.stringify({
-          img: postPicture
-        }),
-    })
-    .then(function() {
-        console.log("Picture Post Success");
-    });
+    let postPicture = base64.replace(/^data:\w+/\w+;base64,/, "");
+    // fetch(postUrl, {
+    //     method: 'POST',
+    //     headers: {'Content-Type': 'application/json' },
+    //     body: JSON.stringify({
+    //       img: postPicture
+    //     }),
+    // })
+    // .then(function() {
+    //     console.log("Picture Post Success");
+    // });
 }
 
 function pictureGet(){
