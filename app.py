cb08e403191ba46612d1516a7d867ab2e16c40f7
Togo555-mp4 <5t1ng20X@gmail.com>
2023-01-23 16:29:25 +0900

    画像の通信

--- app.py
+++ app.py
@@ -46,11 +46,14 @@ def picturePost():
     global startOK
     enc_data = request.json['img']
     dec_data = base64.b64decode(enc_data)
-    with open("python/img/before.jpg", mode='wb') as f:
+    with open("/var/www/html/MOKA/python/img/before.jpg", mode='wb') as f:
         f.write(dec_data)
-    difference = compare_Pic.comparePic("img/before.jpg", "img/after.jpg")
-    with open("python/img/after.jpg", mode='wb') as f:
+
+    difference = compare_Pic.comparePic("/var/www/html/MOKA/python/img/before.jpg", "/var/www/html/MOKA/python/img/after.jpg")
+
+    with open("/var/www/html/MOKA/python/img/after.jpg", mode='wb') as f:
         f.write(dec_data)
+
     if(difference < 5):
         startOK = "OK"
     else:
